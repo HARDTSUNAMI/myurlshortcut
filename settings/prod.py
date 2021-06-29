7b372bad0833df77c6e50bbcb0433b20c75e262d
HARDTSUNAMI <hardtsunamiaf@gmail.com>
2021-06-29 14:56:47 +0300

    Mistake in url configuration of a new address

--- settings/prod.py
+++ settings/prod.py
@@ -1,13 +1,8 @@
 from .base import *
 import dj_database_url
-
-DEBUG = True
+DEBUG = False
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgres://tophsgoaearpea:4edc29f30b0d92fd3cf9ae7ced2c97e396d4c1a1c6b70652616881259f745600@ec2-54-74'
-                '-156 '
-                '-137.eu-west-1.compute.amazonaws.com:5432/d7sjbu2ia00m8n',
-        conn_max_age=600)}
+   'default': dj_database_url.config(conn_max_age=600)}
 
 django_heroku.settings(locals())
